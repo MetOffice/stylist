565596fb4172ca7615c7b5a11df69c1a2845c5f6
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2020-07-17 10:55:41 +0100

    System testing for missing pointer initialisation.

--- system-tests/fortran_rules_test.py
+++ system-tests/fortran_rules_test.py
@@ -15,12 +15,11 @@ from pytest import fixture  # type: ignore
 
 
 class TestFortranRules(object):
-    _RULES = ['bad_character',
-              'missing_implicit',
-              'missing_only',
-              'missing_pointer_init']
-
-    @fixture(scope='class', params=_RULES)
+    @fixture(scope='class',
+             params=['bad_character',
+                     'missing_implicit',
+                     'missing_only',
+                     'missing_pointer_init'])
     def rule(self, request):
         return request.param
 
@@ -49,27 +48,6 @@ class TestFortranRules(object):
                    '-configuration', test_dir / 'configuration.ini',
                    '-style', rule,
                    test_dir / f'{rule}.f90']
-        process = subprocess.run(command,
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE)
-
-        assert process.returncode == 1
-        assert process.stderr.decode('utf-8').strip() \
-            == ''.join(expected_error).strip()
-        assert process.stdout.decode('utf-8').strip() \
-            == ''.join(expected_output).strip()
-
-    def test_all(self):
-        test_dir = Path(__file__).parent / 'fortran'
-
-        expected_output, expected_error \
-            = TestFortranRules._get_expected(test_dir, 'all')
-
-        command = ['python', '-m', 'stylist',
-                   '-configuration', test_dir / 'configuration.ini',
-                   '-style', 'all']
-        for rule in TestFortranRules._RULES:
-            command.append(test_dir / f'{rule}.f90')
         process = subprocess.run(command,
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE)
