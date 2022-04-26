30ac09719aaa19411c25d395c74df53f897d5b5e
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2022-04-26 16:27:39 +0100

    Further tidying up of the configuration script.

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -5,14 +5,16 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 from typing import List
+
+# Modules to document with autodoc are in another directory. That directory is
+# added to 'sys.path' here. The path is best expressed relative to the current
+# directory so needs to be made absolute for use.
+#
 sys.path.insert(0, os.path.abspath('../../source'))
+
 from stylist import __version__ as STYLIST_VERSION
 
 
@@ -22,17 +24,20 @@ project = 'Stylist'
 copyright = '2022, Crown Copyright'
 author = 'Stylist Developers'
 
+# Headline version for archiving purposes.
+#
 version = STYLIST_VERSION.split('-', 1)[0]
 
-# The full version, including alpha/beta/rc tags
+# The full version, including alpha/beta/rc tags. This reflects development.
+#
 release = STYLIST_VERSION
 
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Sphinx extension module names are listed here. They can be extensions coming
+# with Sphinx (named 'sphinx.ext.*') or your custom ones.
+#
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -43,12 +48,15 @@ extensions = [
     'sphinx.ext.coverage'
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+# Paths to directories containing templates. Relative to this directory.
+#
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
+# Filename patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+#
+# These patterns also affect 'html_static_path' and 'html_extra_path'.
+#
 exclude_patterns: List[str] = []
 
 
@@ -58,6 +66,7 @@ autodoc_default_options = {
     'members': True,
     'show-inheritance': True
 }
+
 autoclass_content = 'both'
 
 
@@ -68,7 +77,7 @@ todo_include_todos = True
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
+# The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
@@ -76,6 +85,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+#
 html_static_path = ['_static']
 
 html_logo = '_static/stylist.png'
