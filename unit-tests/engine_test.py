0157078e85b948ade00a5c3cc5970002c25dbd06
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2023-01-31 14:36:00 +0000

    Unit test to stimulate error

--- unit-tests/engine_test.py
+++ unit-tests/engine_test.py
@@ -7,11 +7,14 @@
 """
 Ensures the 'engine' module functions as expected.
 """
+from pathlib import Path
 import tempfile
+from textwrap import dedent
 from typing import List
 
-import stylist.engine
+from stylist.engine import CheckEngine
 from stylist.issue import Issue
+from stylist.rule import Rule
 from stylist.source import SourceTree
 from stylist.style import Style
 
@@ -26,7 +29,7 @@ class _StyleHarness(Style):
         return super(_StyleHarness, self).check(program)
 
 
-def test_all() -> None:
+def test_all_styles() -> None:
     """
     Checks the rules for each registered style see the checked program.
     """
@@ -35,10 +38,34 @@ def test_all() -> None:
         handle.seek(0)
 
         styles = [_StyleHarness(), _StyleHarness()]
-        unit_under_test = stylist.engine.CheckEngine(styles)
+        unit_under_test = CheckEngine(styles)
         unit_under_test.check(handle.name)
 
         assert [program.get_text() for program in styles[0].seen] \
             == ['module teapot\nend module teapot\n\n']
         assert [program.get_text() for program in styles[1].seen] \
             == ['module teapot\nend module teapot\n\n']
+
+
+class LineRule(Rule):
+    def examine(self, subject) -> List[Issue]:
+        return [Issue("With line number", 12, 'beef.f90')]
+
+
+class LinelessRule(Rule):
+    def examine(self, subject) -> List[Issue]:
+        return[Issue("Without line number", None, 'cheese.f90')]
+
+
+def test_issues_with_without_line_numbers(tmp_path: Path) -> None:
+    """
+    Checks that mixing issues with and without line numbers does not cause a
+    crash.
+    """
+    source_file = tmp_path / 'irreverent.f90'
+    source_file.write_text("It doesn't matter what's in here.")
+
+    style = Style(LineRule(), LinelessRule())
+    test_unit = CheckEngine([style])
+    issues = test_unit.check(str(source_file))
+    assert issues == []
