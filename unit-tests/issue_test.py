a72c8d6999f2865d0fcb8ecc0b2a6da15767e0c3
MatthewHambley <MatthewHambley@users.noreply.github.com>
2023-04-21 10:57:08 +0100

    Properly support sorting lists of issues (#105)
    * Unit test to stimulate error
    
    * Make Issue sortable
    
    * Remove left overs from when problem was thought to be elsewhere.
    
    * Styling
    
    * More styling
    
    * Updated issue sorting to use pathlib.
    
    * Deflake

--- unit-tests/issue_test.py
+++ unit-tests/issue_test.py
@@ -7,13 +7,33 @@
 """
 Ensures the Issue object functions as expected.
 """
+from pathlib import Path
 
-import stylist.issue
+from stylist.issue import Issue
 
 
 def test_constructor() -> None:
     """
     Checks the constructor works.
     """
-    unit_under_test = stylist.issue.Issue('Teapot cheese')
+    unit_under_test = Issue('Teapot cheese')
     assert str(unit_under_test) == 'Teapot cheese'
+
+
+def test_sortability() -> None:
+    """
+    Checks a list of Item can be sorted.
+    """
+    test_list = [Issue("With line number and file", 12, Path('cheese.txt')),
+                 Issue("Without line number or file"),
+                 Issue("With line number but not file", 27),
+                 Issue("Without line number but with file",
+                       filename=Path('beef.txt')),
+                 Issue("With everything again", 39, Path('cheese.txt'))]
+    test_list.sort()
+    assert [str(issue) for issue in test_list] \
+           == ['Without line number or file',
+               '27: With line number but not file',
+               'beef.txt: Without line number but with file',
+               'cheese.txt: 12: With line number and file',
+               'cheese.txt: 39: With everything again']
